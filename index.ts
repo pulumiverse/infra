7bb12e79dc07fc9ba535850b880132582ae619cf
Ringo De Smet <ringo@de-smet.name>
2022-03-08 20:13:45 +0100

    Importing the infra repo into Pulumi state.
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-// 
+import * as github from "@pulumi/github";
+
+const repo = new github.Repository("infra",
+    {
+        name: 'infra',
+        hasDownloads: false,
+        hasIssues: true,
+        hasProjects: false,
+        hasWiki: false,
+        visibility: 'public',
+    },
+    {
+        import: 'infra'
+    }
+);
