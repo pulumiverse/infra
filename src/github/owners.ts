f2bbf91c0314bbab99c71081d7d32e77fd8e6362
Ringo De Smet <ringo@de-smet.name>
2022-03-15 23:01:55 +0100

    Refactored to collections.
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

--- src/github/owners.ts
+++ src/github/owners.ts
@@ -1,45 +1,19 @@
 import * as github from "@pulumi/github";
 
-const ringods = new github.Membership('ringo_de_smet',
-    {
-        username: 'ringods',
-        role: 'admin'
-    }
-)
+const owners = [
+    'ringods',
+    'cobraz',
+    'stack72',
+    'usrbinkat',
+    'rawkode',
+    'tenwit'
+];
 
-const cobraz = new github.Membership('simen_aw_olsen',
-    {
-        username: 'cobraz',
-        role: 'admin'
-    }
-)
-
-const stack72 = new github.Membership('paul_stack',
-    {
-        username: 'stack72',
-        role: 'admin'
-    }
-)
-
-const usrbinkat = new github.Membership('kathryn_morgan',
-    {
-        username: 'usrbinkat',
-        role: 'admin'
-    }
-)
-
-const rawkode = new github.Membership('david_flanagan',
-    {
-        username: 'rawkode',
-        role: 'admin'
-    }
-)
-
-const tenwit = new github.Membership('paul_hicks',
-    {
-        username: 'tenwit',
-        role: 'admin'
-    }
-)
-
-export const all = [ ringods, cobraz, stack72, usrbinkat, rawkode, tenwit ]
+export const all = owners.map((userid) => {
+    return new github.Membership(`owner_${userid}`,
+        {
+            username: userid,
+            role: 'admin'
+        }
+    )
+})
--- src/github/teams.ts
+++ src/github/teams.ts
@@ -8,44 +8,22 @@ const board = new github.Team('governance_board',
     }
 )
 
-const board_ringods = new github.TeamMembership('board_ringods',
-    {
-        teamId: board.id,
-        username: 'ringods',
-        role: 'maintainer'
-    }
-)
+const boardmembers = [
+    'ringods',
+    'cobraz',
+    'usrbinkat',
+    'rawkode',
+    'tenwit'
+];
 
-const board_cobraz = new github.TeamMembership('board_cobraz',
-    {
-        teamId: board.id,
-        username: 'cobraz',
-        role: 'maintainer'
-    }
-)
+boardmembers.map((userid) => {
+    const board_ringods = new github.TeamMembership(`board_${userid}`,
+        {
+            teamId: board.id,
+            username: userid,
+            role: 'maintainer'
+        }
+    )
+})
 
-const board_usrbinkat = new github.TeamMembership('board_usrbinkat',
-    {
-        teamId: board.id,
-        username: 'usrbinkat',
-        role: 'maintainer'
-    }
-)
-
-const board_rawkode = new github.TeamMembership('board_rawkode',
-    {
-        teamId: board.id,
-        username: 'rawkode',
-        role: 'maintainer'
-    }
-)
-
-const board_tenwit = new github.TeamMembership('board_tenwit',
-    {
-        teamId: board.id,
-        username: 'tenwit',
-        role: 'maintainer'
-    }
-)
-
-export const all_teams = [ board ]
+export const all_teams = [board]
