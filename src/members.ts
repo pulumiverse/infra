45154b91741cd76bf58b4355265943f1e7185bb6
Simen A. W. Olsen <so@bjerk.io>
2022-06-11 14:39:26 +0200

    add invalid member

--- src/members.ts
+++ src/members.ts
@@ -1,11 +1,16 @@
 import { Member } from "./types";
 
 export const members: Member[] = [
+    // Valid member
     {
         username: 'ringods',
         role: 'admin',
         membershipImport: 'owner_ringods',
         teamMembershipImport: 'board_ringods',
         maintainer: ['governance_board']
-    }
+    },
+    // Invalid member
+    {
+        user: 'ghost'
+    },
 ];
