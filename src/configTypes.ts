245f7cc79e2fc5164d66efe9601def62a64f95b9
Steve Morgan <steve@rebelops.io>
2024-03-07 14:00:17 -0500

    fix: fix diff issue on deprecated gh setting

--- src/configTypes.ts
+++ src/configTypes.ts
@@ -1,41 +1,46 @@
-import * as RT from "runtypes";
-import {Static} from "runtypes/lib/runtype";
+import * as RT from 'runtypes';
+import { Static } from 'runtypes/lib/runtype';
 
 export const Team = RT.Record({
-    name: RT.String,
-    description: RT.String,
-})
+  name: RT.String,
+  description: RT.String,
+});
 
 export const Member = RT.Record({
-    username: RT.String,
-    role: RT.String.optional(),
-    membershipImport: RT.String.optional(),
-    teamMembershipImport: RT.String.optional(),
-    pull: RT.Array(RT.String).optional(),
-    triage: RT.Array(RT.String).optional(),
-    push: RT.Array(RT.String).optional(),
-    maintain: RT.Array(RT.String).optional(),
-    admin: RT.Array(RT.String).optional(),
-})
+  username: RT.String,
+  role: RT.String.optional(),
+  membershipImport: RT.String.optional(),
+  teamMembershipImport: RT.String.optional(),
+  pull: RT.Array(RT.String).optional(),
+  triage: RT.Array(RT.String).optional(),
+  push: RT.Array(RT.String).optional(),
+  maintain: RT.Array(RT.String).optional(),
+  admin: RT.Array(RT.String).optional(),
+});
 
 export const Labels = RT.Record({
-    name: RT.String,
-    color: RT.String,
-    description: RT.String.optional(),
-})
+  name: RT.String,
+  color: RT.String,
+  description: RT.String.optional(),
+});
 
 export const Repository = RT.Record({
-    name: RT.String,
-    description: RT.String,
-    type: RT.Union(RT.Literal('administrative'), RT.Literal('provider'), RT.Literal('information')),
-    teams: RT.Array(RT.String).optional(),
-    topics: RT.Array(RT.String).optional(),
-    labels: RT.Array(Labels).optional(),
-    import: RT.Boolean.optional(),
-    template: RT.String.optional(),
-    removable: RT.Boolean.optional(),
-})
+  name: RT.String,
+  description: RT.String,
+  type: RT.Union(
+    RT.Literal('administrative'),
+    RT.Literal('provider'),
+    RT.Literal('information'),
+  ),
+  teams: RT.Array(RT.String).optional(),
+  topics: RT.Array(RT.String).optional(),
+  labels: RT.Array(Labels).optional(),
+  hasDownloads: RT.Boolean.optional(),
+  import: RT.Boolean.optional(),
+  template: RT.String.optional(),
+  removable: RT.Boolean.optional(),
+});
 
-export type Team = Static<typeof Team>
-export type Member = Static<typeof Member>
-export type Repository = Static<typeof Repository>
+export type Team = Static<typeof Team>;
+export type Member = Static<typeof Member>;
+export type Repository = Static<typeof Repository>;
